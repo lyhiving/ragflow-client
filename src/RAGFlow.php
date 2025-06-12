62aae67b153f9cef436d13c44d61f041f3481ad5
s9 <lyhiving@qq.com>
2025-06-12 17:57:19 +0800

    fix

--- src/RAGFlow.php
+++ src/RAGFlow.php
@@ -14,6 +14,7 @@ final class RAGFlow
     {
         return self::factory()
             ->withApiKey($apiKey)
+            ->withApiKey($apiKey)
             ->withOrganization($organization)
             ->withProject($project)
             ->withHttpHeader('RAGFlow-Beta', 'assistants=v2')
